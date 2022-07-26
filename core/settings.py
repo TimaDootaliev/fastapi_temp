9965866ab05e3d7a4a84ae196bac533d8fdcbb73
TimaDootaliev <timur.dootaliev.td@gmail.com>
2022-07-26 15:59:59 +0600

    setting up

---
+++ core/settings.py
@@ -0,0 +1,14 @@
+from decouple import config
+
+class Settings:
+    DB_ENGINE = config('DB_ENGINE', default='sqlite')
+    DB_NAME = config('DB_NAME', default='db.sqlite3')
+    DB_USER = config('DB_USER', default='')
+    DB_PASSWORD = config('DB_PASSWORD', default='')
+    DB_HOST = config('DB_HOST', default='')
+    DB_PORT = config('DB_PORT', default='')
+
+    SECRET_KEY = config('SECRET_KEY', default='secret_key')
+
+    DEBUG = config('DEBUG', default=False, cast=bool)
+    DB_URL = '{}://{}:{}@{}:{}/{}'.format(DB_ENGINE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
